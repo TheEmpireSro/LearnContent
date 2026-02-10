709317e82e3db0df08a037e90b34956b3dce7f42
TheEmpireSro <skeliorthepro@gmail.com>
2026-02-10 20:11:44 +0100

    test

---
+++ 06_Plus.js
@@ -0,0 +1,14 @@
+"use strict"
+
+// console.log(5 + 6)
+
+// console.log("Das Ergebnis ist 11!" + " Das war super!")
+
+// console.log("5" + "6")
+
+// console.log (true + true)
+
+let amount = 10 + "1.9" // das hier ist falsch int + string
+console.log(amount)
+
+// Plus Operator addiert und verbindet strings
